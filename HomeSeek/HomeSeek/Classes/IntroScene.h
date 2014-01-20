c7141a871250ea642100f77a2a0ce62e33d1e2f7
Maksat Aman <maksat@peakgames.net>
2014-01-20 11:49:37 +0200

    Created new xcode3 project

---
+++ HomeSeek/HomeSeek/Classes/IntroScene.h
@@ -0,0 +1,30 @@
+//
+//  IntroScene.h
+//  HomeSeek
+//
+//  Created by Maksat Aman on 1/20/14.
+//  Copyright Peak games 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using cocos2d-v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The intro scene
+ *  Note, that scenes should now be based on CCScene, and not CCLayer, as previous versions
+ *  Main usage for CCLayer now, is to make colored backgrounds (rectangles)
+ *
+ */
+@interface IntroScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (IntroScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
---
+++ HomeSeek/HomeSeek/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HomeSeek
+//
+//  Created by Maksat Aman on 1/20/14.
+//  Copyright Peak games 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
